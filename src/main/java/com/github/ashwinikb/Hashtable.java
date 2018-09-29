85e23f3d8e43efe2a9e882c917b06292fdea9df8
ashwinikb <ashwini.kbasavaraju@gmail.com>
2018-09-29 15:28:06 -0700

    Cleanup.

--- src/main/java/com/github/ashwinikb/Hashtable.java
+++
@@ -1,8 +0,0 @@
-package com.github.ashwinikb;
-
-public class Hashtable {
-
-    public void addElement(){
-        CustomHashTable<String, Integer> map = new CustomHashTable<>();
-    }
-}
--- src/main/java/com/github/ashwinikb/LinkedList.java
+++
@@ -1,9 +0,0 @@
-package com.github.ashwinikb;
-
-public class LinkedList {
-
-
-    public void customList(){
-        CustomList<String> list = new CustomList<String>();
-    }
-}
